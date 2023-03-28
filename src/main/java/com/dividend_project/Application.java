21c1a9a43333449ea09eaf357f6eb32283fe7530
unknown <meeji222@naver.com>
2023-03-28 16:54:11 +0900

    Feat: Scheduler 적용 추가

--- src/main/java/com/dividend_project/Application.java
+++ src/main/java/com/dividend_project/Application.java
@@ -5,9 +5,11 @@ import org.apache.commons.collections4.Trie;
 import org.apache.commons.collections4.trie.PatriciaTrie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
--- src/main/java/com/dividend_project/persist/DividendRepository.java
+++ src/main/java/com/dividend_project/persist/DividendRepository.java
@@ -5,10 +5,11 @@ import com.dividend_project.persist.entitiy.DividendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DividendRepository extends JpaRepository<DividendEntity,Long> {
     List<DividendEntity> findAllByCompanyId(Long companyId);
-
+    boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
 }
